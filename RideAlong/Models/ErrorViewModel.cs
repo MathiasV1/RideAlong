0173cd114115c24edbd875b062a53ddc3ed886b1
Mathias Vansevenant <mathias.vansevenant@student.hogent.be>
2019-11-22 22:02:06 +0100

    Add project files.

---
+++ RideAlong/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RideAlong.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
