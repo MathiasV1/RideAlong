e24e70be58af51f8a21e1f24fff09e7d2724b63a
Mathias Vansevenant <mathias.vansevenant@student.hogent.be>
2019-11-22 22:20:26 +0100

    Add person class

--- RideAlong/Models/Domain/Person.cs
+++ RideAlong/Models/Domain/Person.cs
@@ -7,5 +7,15 @@ namespace RideAlong.Models.Domain
 {
     public class Person
     {
+        private string _voornaam;
+        private string _naam;
+        private string _telefoonnummer;
+
+        public Person(string voornaam, string naam, string telefoonNummer)
+        {
+            _voornaam = voornaam;
+            _naam = naam;
+            _telefoonnummer = telefoonNummer;
+        }
     }
 }
