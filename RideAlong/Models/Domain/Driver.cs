b8f554b447c27c509a2fe6eee81a1e9c3c2f17c6
Jorik Van Damme <jorik.vandamme@student.hogent.be>
2019-11-23 00:31:52 +0100

    Implementeer Locatie

--- RideAlong/Models/Domain/Driver.cs
+++ RideAlong/Models/Domain/Driver.cs
@@ -1,7 +1,47 @@
 ﻿namespace RideAlong.Models.Domain {
     public class Driver : Person {
-        private int _aantalRitten;
+
+        private int _aantalPassagiers;
+        public int AantalPassagiers {
+            get {
+                return _aantalPassagiers;
+            }
+            private set {
+                if (value >= 0) {
+                    _aantalPassagiers = value;
+                }
+            }
+        }
+
         private float _rating;
+        public float Rating {
+            get {
+                return _rating;
+            }
+            private set {
+                if(value >= 0) {
+                    _rating = value;
+                }
+            } 
+        }
+
         private float _maxDetour;
+        public float MaxDetour {
+            get {
+                return _maxDetour;
+            }
+            private set {
+                if (value >= 0) {
+                    _maxDetour = value;
+                }
+            }
+        }
+
+
+        public Driver(string voornaam, string naam, string telefoonnr, int aantalPassagiers, float rating, float maxDetour) : base(voornaam, naam, telefoonnr) {
+            AantalPassagiers = aantalPassagiers;
+            Rating = rating;
+            MaxDetour = maxDetour;
+        }
     }
 }
--- RideAlong/Models/Domain/Location.cs
+++ RideAlong/Models/Domain/Location.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿namespace RideAlong.Models.Domain {
+    public class Location {
 
-namespace RideAlong.Models.Domain
-{
-    public class Location
-    {
     }
 }
