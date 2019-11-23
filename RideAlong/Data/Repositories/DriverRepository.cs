88b06e6b95d27f4835ad639fcc2573367e483ace
Mathias Vansevenant <mathias.vansevenant@student.hogent.be>
2019-11-23 04:03:08 +0100

    .

--- RideAlong/Data/Repositories/DriverRepository.cs
+++ RideAlong/Data/Repositories/DriverRepository.cs
@@ -1,5 +1,5 @@
 ﻿using RideAlong.Models.Domain;
-using RideAlong.Models.Persistentie;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +11,7 @@ namespace RideAlong.Data.Repositories
     {
         //private readonly ApplicationDbContext _dbContext;
         //private readonly DBSet<Person> _drivers;
-        PersonMapper _pm;
+        
 
         public DriverRepository()
         {
