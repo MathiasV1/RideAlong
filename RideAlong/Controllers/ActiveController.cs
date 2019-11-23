3e086d4c21843e5dc25aef908b9fa0ae90033381
Jorik Van Damme <jorik.vandamme@student.hogent.be>
2019-11-23 01:58:25 +0100

    Add ActiveController + View

--- RideAlong/Controllers/ActiveController.cs
+++ RideAlong/Controllers/ActiveController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RideAlong.Models.Domain;
 
 namespace RideAlong.Controllers
 {
-    public class DefaultController : Controller
+    public class ActiveController : Controller
     {
+
+
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ActiveDriverView(int driverID, List<Passenger> passengers) {
+            //ViewData["Driver"] = ;
+            return View(passengers);
         }
     }
 }
